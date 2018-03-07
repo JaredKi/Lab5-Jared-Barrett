0951584ed00cb7db0de61c4ce5dd59c57a18d981
JaredKi <36872557+JaredKi@users.noreply.github.com>
2018-03-06 21:17:44 -0600

    systickdone

--- SysTick.c
+++ SysTick.c
@@ -7,12 +7,16 @@
 #define NVIC_ST_CURRENT_R   (*((volatile unsigned long *)0xE000E018))
 void SysTick_Init(void){
 // you write this as part of Lab 5
-  
+  NVIC_ST_CTRL_R = 0;               // disable SysTick during setup
+  NVIC_ST_CTRL_R = 0x00000005;      // enable SysTick with core clock
 }
 // The delay parameter is in units of the 80 MHz core clock. (12.5 ns)
 void SysTick_Wait(uint32_t delay){
 // you write this as part of Lab 5
-  
+  NVIC_ST_RELOAD_R = delay-1;  // number of counts to wait
+  NVIC_ST_CURRENT_R = 0;       // any value written to CURRENT clears
+  while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for count flag
+  } 
 }
 
 // Time delay using busy wait.
@@ -20,6 +24,8 @@ void SysTick_Wait(uint32_t delay){
 // 10000us equals 10ms
 void SysTick_Wait10ms(uint32_t delay){
 // you write this as part of Lab 5
-  
+  uint32_t i;
+  for(i=0; i<delay; i++){
+    SysTick_Wait(800000);  // wait 10ms
 }
-
+}
